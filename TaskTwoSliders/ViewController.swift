4de888a212849cc92b0fb9a435a6279eac30673a
Alex Sobolev <31108@mail.ru>
2022-05-06 21:47:51 +0300

    Initial Commit

---
+++ TaskTwoSliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskTwoSliders
+//
+//  Created by Александр Соболев on 06.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
