f1b7edefcfb983c3ece3d06fe16e1712dbaa65a7
Alex Sobolev <31108@mail.ru>
2022-05-06 21:57:53 +0300

    Setup outlet for main view and corner radius

--- TaskTwoSliders/ViewController.swift
+++ TaskTwoSliders/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainViewLabel: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mainViewLabel.layer.cornerRadius = 10
     }
 
 
