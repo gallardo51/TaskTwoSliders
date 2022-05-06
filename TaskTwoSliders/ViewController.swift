8d875122b524108be130e21b7b7f25da7b43ede5
Alex Sobolev <31108@mail.ru>
2022-05-06 22:34:49 +0300

    Setup outlets for all view elements

--- TaskTwoSliders/ViewController.swift
+++ TaskTwoSliders/ViewController.swift
@@ -11,6 +11,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mainViewLabel: UIView!
     
+    @IBOutlet weak var redViewLabel: UILabel!
+    @IBOutlet weak var redCountLabel: UILabel!
+    
+    
+    @IBOutlet weak var greenViewLabel: UILabel!
+    @IBOutlet weak var greenCountLabel: UILabel!
+    
+    @IBOutlet weak var blueViewLabel: UILabel!
+    @IBOutlet weak var blueCountLabel: UILabel!
+    
+    @IBOutlet weak var redSliderLabel: UISlider!
+    @IBOutlet weak var greenSliderLabel: UISlider!
+    @IBOutlet weak var blueSliderLabel: UISlider!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
