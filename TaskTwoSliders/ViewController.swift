01892482b246e5a4ac56e08642873d06416a584a
Alex Sobolev <31108@mail.ru>
2022-05-15 11:08:40 +0300

    Setup red

--- TaskTwoSliders/ViewController.swift
+++ TaskTwoSliders/ViewController.swift
@@ -31,7 +31,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // View Label
         mainViewLabel.layer.cornerRadius = 10
+        
+        // Red
+        redSliderLabel.value = 1
+        redSliderLabel.minimumValue = 0
+        redSliderLabel.maximumValue = 1
+        redCountLabel.text = String(redSliderLabel.value)
+        
+        // Green
+        
+        // Blue View Label
+        
+        // Red Slider Label
+        
+        // Green Slider Label
+        
+        // Blue Slider Label
+        
+        
     }
 
 
