d508935113b8a970cdae5d39e58cbc26c4382fdf
Alex Sobolev <31108@mail.ru>
2022-08-02 15:13:43 +0300

    Rename sliders

--- TaskTwoSliders/ColorViewController.swift
+++ TaskTwoSliders/ColorViewController.swift
@@ -24,9 +24,9 @@ class ColorViewController: UIViewController {
     @IBOutlet weak var blueViewLabel: UILabel!
     @IBOutlet weak var blueCountLabel: UILabel!
     
-    @IBOutlet weak var redSliderLabel: UISlider!
-    @IBOutlet weak var greenSliderLabel: UISlider!
-    @IBOutlet weak var blueSliderLabel: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     
     @IBOutlet weak var redTextField: UITextField!
     @IBOutlet weak var greenTextField: UITextField!
@@ -45,39 +45,39 @@ class ColorViewController: UIViewController {
         mainViewLabel.backgroundColor = viewColor
         
         // Red
-        redSliderLabel.value = 0.15
-        redSliderLabel.minimumValue = 0
-        redSliderLabel.maximumValue = 1
-        redCountLabel.text = String(redSliderLabel.value)
-        redSliderLabel.tintColor = .red
+        redSlider.value = 0.15
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redCountLabel.text = String(redSlider.value)
+        redSlider.tintColor = .red
         
         
         
         // Green
-        greenSliderLabel.value = 0.35
-        greenSliderLabel.minimumValue = 0
-        greenSliderLabel.maximumValue = 1
-        greenCountLabel.text = String(greenSliderLabel.value)
-        greenSliderLabel.tintColor = .green
+        greenSlider.value = 0.35
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenCountLabel.text = String(greenSlider.value)
+        greenSlider.tintColor = .green
         
         
         // Blue
-        blueSliderLabel.value = 0.55
-        blueSliderLabel.minimumValue = 0
-        blueSliderLabel.maximumValue = 1
-        blueCountLabel.text = String(blueSliderLabel.value)
+        blueSlider.value = 0.55
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueCountLabel.text = String(blueSlider.value)
     
     }
 
     @IBAction func changeSliderAction() {
-        redCountLabel.text = String(format: "%.2f", redSliderLabel.value)
-        let redSliderValue = CGFloat(redSliderLabel.value)
+        redCountLabel.text = String(format: "%.2f", redSlider.value)
+        let redSliderValue = CGFloat(redSlider.value)
         
-        greenCountLabel.text = String(format: "%.2f", greenSliderLabel.value)
-        let greenSliderValue = CGFloat(greenSliderLabel.value)
+        greenCountLabel.text = String(format: "%.2f", greenSlider.value)
+        let greenSliderValue = CGFloat(greenSlider.value)
         
-        blueCountLabel.text = String(format: "%.2f", blueSliderLabel.value)
-        let blueSliderValue = CGFloat(blueSliderLabel.value)
+        blueCountLabel.text = String(format: "%.2f", blueSlider.value)
+        let blueSliderValue = CGFloat(blueSlider.value)
         
         
         mainViewLabel.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: 1)
@@ -86,9 +86,9 @@ class ColorViewController: UIViewController {
     private func setSliders() {
         let ciColor = CIColor(color: viewColor)
         
-        redSliderLabel.value = Float(ciColor.red)
-        greenSliderLabel.value = Float(ciColor.green)
-        blueSliderLabel.value = Float(ciColor.blue)
+        redSlider.value = Float(ciColor.red)
+        greenSlider.value = Float(ciColor.green)
+        blueSlider.value = Float(ciColor.blue)
     }
     
     @IBAction func doneButtonPressed(_ sender: UIButton) {
