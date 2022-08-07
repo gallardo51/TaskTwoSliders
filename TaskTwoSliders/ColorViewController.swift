232dfd7e9f94424c64581317211ca9095d1bda6a
Alex Sobolev <31108@mail.ru>
2022-08-07 17:41:17 +0300

    Setup storyboard text field delegate

--- TaskTwoSliders/ColorViewController.swift
+++ TaskTwoSliders/ColorViewController.swift
@@ -32,8 +32,11 @@ class ColorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // View Label
         mainViewLabel.layer.cornerRadius = 10
+        
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextField.delegate = self
         
         redSlider.tintColor = .red
         greenSlider.tintColor = .green
@@ -110,5 +113,49 @@ extension ColorViewController {
     
     private func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
+    }
+    
+    @objc private func didTapDone() {
+        view.endEditing(true)
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension ColorViewController: UITextFieldDelegate {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        guard let text = textField.text else { return }
+        
+        if let currentValue = Float(text) {
+            switch textField {
+            case redTextField:
+                redSlider.setValue(currentValue, animated: true)
+                setValue(for: redCountLabel)
+            case greenTextField:
+                greenSlider.setValue(currentValue, animated: true)
+                setValue(for: greenCountLabel)
+            default:
+                blueSlider.setValue(currentValue, animated: true)
+                setValue(for: blueCountLabel)
+            }
+            
+            setNewColor()
+            return
+        }
+        
+        showAlert(title: "Wrong format!", message: "Please enter correct value")
     }
 }
